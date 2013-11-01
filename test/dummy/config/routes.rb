568de1478d23db6546ffdd6a96b59a4ecf8e7d0a
David Southard <nacengineer@gmail.com>
2013-11-01 15:25:26 -0500

    remove test

--- test/dummy/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-
-  match "/home" => "default#index"
-  mount HoneyBadger::Engine => "/honey_badger"
-  match "/" => "default#index"
-end
--- test/honey_badger_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class HoneyBadgerTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, HoneyBadger
-  end
-end
