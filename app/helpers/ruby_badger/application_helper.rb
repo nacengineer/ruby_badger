dfdaed2dd4831a7b22ff9804b0d5617c2f8cadbc
David Southard <dsouthard@wisc.edu>
2018-03-09 11:12:24 -0600

    add git info

--- app/helpers/ruby_badger/application_helper.rb
+++ app/helpers/ruby_badger/application_helper.rb
@@ -1,10 +1,20 @@
 module RubyBadger
   module ApplicationHelper
-    def ruby_badger(text=Rails.env)
+    def ruby_badger(text = Rails.env, git_info = false)
+    	text = Rails.env if text.nil?
       content_tag :div, :class => "corner-banner ruby-badger #{Rails.env}" do
-        text = text.join('<br/>') if text.instance_of?(Array)
-        text.html_safe
+      	if text.instance_of?(Array)
+        	text << [git_branch_info] if git_info
+	      	text.join('<br/>').html_safe
+        else
+        	text << '<br/>' << git_branch_info if git_info
+        	text.html_safe
+        end
       end
     end
+
+    def git_branch_info
+	    `git rev-parse --abbrev-ref HEAD`.chomp
+	  end
   end
 end
